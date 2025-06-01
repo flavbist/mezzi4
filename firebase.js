524bc5e27ef329fcdc814f3817e30775363a4688
flavbist <flavbist@gmail.com>
2025-06-01 22:08:43 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,18 @@
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-storage.js";
 
-// Configurazione Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAQcE3-MInQ0cqfrEaAZc6ZlPYkidPijog",
-    authDomain: "mezziusati-7f3af.firebaseapp.com",
-    projectId: "mezziusati-7f3af",
-    storageBucket: "mezziusati-7f3af.appspot.com",
-    messagingSenderId: "250616706905",
-    appId: "1:250616706905:web:98669a6bc4e6a1a4cffe48"
+  apiKey: "AIzaSyAQcE3-MInQ0cqfrEaAZc6ZlPYkidPijog",
+  authDomain: "mezziusati-7f3af.firebaseapp.com",
+  projectId: "mezziusati-7f3af",
+  storageBucket: "mezziusati-7f3af.appspot.com", // 🟢 Corretto
+  messagingSenderId: "250616706905",
+  appId: "1:250616706905:web:98669a6bc4e6a1a4cffe48"
 };
 
-// Inizializza Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-// Esporta le funzioni necessarie
-export { db, collection, getDocs, addDoc, deleteDoc, doc };
+export { db, storage, collection, getDocs, addDoc, deleteDoc, doc };
