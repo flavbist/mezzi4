8148f8e186094cb8f0a02ac5e7d8a63c69628aad
flavbist <flavbist@gmail.com>
2025-06-01 22:36:07 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from "https
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQcE3-MInQ0cqfrEaAZc6ZlPYkidPijog",
-    authDomain: "mezziusati-7f3af.firebaseapp.com",
-    projectId: "mezziusati-7f3af",
-    storageBucket: "mezziusati-7f3af.appspot.com", // 🟢 Corretto!
-    messagingSenderId: "250616706905",
-    appId: "1:250616706905:web:98669a6bc4e6a1a4cffe48"
+  apiKey: "AIzaSyA1SDyweRO26f-FttilUouHoWU9GmFO_3g",
+  authDomain: "mezzi-82b21.firebaseapp.com",
+  projectId: "mezzi-82b21",
+  storageBucket: "mezzi-82b21.appspot.com", // 🟢 Corretto!
+  messagingSenderId: "502246532238",
+  appId: "1:502246532238:web:1d26e5c6be0a89f0df15df"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -16,3 +16,7 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { db, storage, collection, getDocs, addDoc, deleteDoc, doc };
+
+
+
+
