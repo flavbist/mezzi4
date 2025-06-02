8f7f8b9cea5ba0543354b4b0f092967586be30cd
flavbist <flavbist@gmail.com>
2025-06-02 10:18:37 +0200

    Create consultazione.js

---
+++ consultazione.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scheda Mezzo</title>
+    <link rel="stylesheet" href="style.css">
+    <script type="module" src="firebase.js"></script>
+    <script type="module" src="consultazione.js"></script>
+</head>
+<body>
+    <div id="codice-mezzo" style="font-weight:bold; color:#fff; background:#1976d2; padding:14px; border-radius:7px; font-size:1.4em; margin-bottom:25px; letter-spacing:2px; text-align:center;"></div>
+    <div id="scheda-mezzo">
+        <!-- Dettagli del mezzo verranno inseriti qui da consultazione.js -->
+    </div>
+    <a href="index.html" class="btn">Torna Indietro</a>
+</body>
+</html>
